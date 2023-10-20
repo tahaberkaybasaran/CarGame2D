079fac2050b251f7492414f881578a40f3328562
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-10-20 16:17:56 +0300

    adding some sprite and script

---
+++ Assets/Print_to_console.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Print_to_console : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello unity taha");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
