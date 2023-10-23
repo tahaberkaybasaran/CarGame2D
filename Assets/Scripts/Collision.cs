cb8238736b1290d42690dc180f752ba58971e578
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-10-23 23:26:42 +0300

    Speed boost and speed down

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,16 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    Boolean isPickedUp;
+    [SerializeField] float pickingDelayTime = 0.5f;
+
+    [SerializeField] Color32 pickedUpColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 deliveredColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRendererCar;
+
+    void Start()
+    {
+        spriteRendererCar = GetComponent<SpriteRenderer>();
+    }
+
+
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Car Touched Something!!!");
+        Debug.Log("Car Crash!!!");
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("On Trigger Toucher!!!");
+        if (other.tag == "Customer" && !isPickedUp)
+        {
+            Debug.Log("The package picked up!");
+            isPickedUp = true;
+            spriteRendererCar.color = pickedUpColor;
+            Destroy(other.gameObject, pickingDelayTime);
+        }
+        else if (other.tag == "FinishPoint" && isPickedUp)
+        {
+            Debug.Log("The package is delivered!");
+            isPickedUp = false;
+            spriteRendererCar.color = deliveredColor;
+        }
+
     }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,12 +6,15 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float carRotateSpeed = 300.0f;
     [SerializeField] float carTransformSpeed = 20.0f;
+    [SerializeField] float boostSpeed = 40.0f;
+    [SerializeField] float slowSpeed = 10.0f;
     void Start()
     {
 
     }
 
     // Update is called once per frame
+
     void Update()
     {
         float carRotateSpeedAmount = Input.GetAxis("Horizontal") * carRotateSpeed * Time.deltaTime;
@@ -19,5 +22,29 @@ public class Driver : MonoBehaviour
         // Debug.Log(carRotateSpeedAmount);
         transform.Rotate(0, 0, -carRotateSpeedAmount);
         transform.Translate(0, carSpeedRotation, 0);
+
+
+    }
+
+    // void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     carTransformSpeed = slowSpeed;
+    // }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SpeedBoost")
+        {
+            carTransformSpeed = boostSpeed;
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "SpeedDown")
+        {
+            carTransformSpeed = slowSpeed;
+        }
     }
 }
